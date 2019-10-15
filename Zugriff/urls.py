d828471f6bafdc4e31becd31be95a4fa3d83d226
lobomaudocerrado <lobomaudocerrado@gmail.com>
2019-10-15 13:17:39 -0300

    Acrescentado o logout.

--- Zugriff/urls.py
+++ Zugriff/urls.py
@@ -2,9 +2,10 @@
 
 from django.conf.urls import url
 from django.urls import path
-from django.contrib.auth.views import LoginView # view do django.
+from django.contrib.auth.views import LoginView, LogoutView # view do django.
 
 urlpatterns = [
 
     path('Einlass/', LoginView.as_view(template_name = 'Zugriff/Einlass.html'), name='login'),
+    path('Ausgang/', LogoutView.as_view(next_page='index'), name='logout'),
 ]
