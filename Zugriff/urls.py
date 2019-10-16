ce19c25fdea485ad1ce7e2f415bb8e891a7265d1
lobomaudocerrado <lobomaudocerrado@gmail.com>
2019-10-16 18:07:56 -0300

    Incluído o painel de usuário

--- Zugriff/urls.py
+++ Zugriff/urls.py
@@ -3,11 +3,12 @@
 from django.conf.urls import url
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView # view do django.
-from .views import NeuerBenutzerView
+from .views import IndexView, NeuerBenutzerView
 
 urlpatterns = [
 
+    path('',IndexView.as_view(), name='index'),
     path('Einlass/', LoginView.as_view(template_name = 'Zugriff/Einlass.html'), name='login'),
     path('Ausgang/', LogoutView.as_view(next_page='index'), name='logout'),
-    path('neuerBenutzer/', NeuerBenutzerView.as_view(), name='neuerBenutzer')
+    path('neuerBenutzer/', NeuerBenutzerView.as_view(), name='neuerBenutzer'),
 ]
--- Zugriff/views.py
+++ Zugriff/views.py
@@ -3,10 +3,15 @@
 from django.shortcuts import render
 
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import UserAdminCreationForm
+
+class IndexView(LoginRequiredMixin, TemplateView):
+
+    template_name = 'Zugriff/index.html'
 
 class NeuerBenutzerView(CreateView):
     """ Eine View für die Registrierung neuer Benutzer. """
