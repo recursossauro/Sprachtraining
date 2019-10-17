b588bb31d66213b72bacbf3dd0fef4a43ebaa0fc
lobomaudocerrado <lobomaudocerrado@gmail.com>
2019-10-17 13:35:44 -0300

    Beinhaltet die Änderung der Benutzerregistrierungsdaten.

--- Zugriff/urls.py
+++ Zugriff/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import url
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView # view do django.
-from .views import IndexView, NeuerBenutzerView
+from .views import IndexView, NeuerBenutzerView, AndernBenutzerView
 
 urlpatterns = [
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('Einlass/', LoginView.as_view(template_name = 'Zugriff/Einlass.html'), name='login'),
     path('Ausgang/', LogoutView.as_view(next_page='index'), name='logout'),
     path('neuerBenutzer/', NeuerBenutzerView.as_view(), name='neuerBenutzer'),
+    path('andernBenutzer/', AndernBenutzerView.as_view(), name='andernBenutzer'),
 ]
--- Zugriff/views.py
+++ Zugriff/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render
 
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -19,3 +19,12 @@ class NeuerBenutzerView(CreateView):
     template_name =  'Zugriff/neuer_Benutzer.html'
     model         = get_user_model()
     success_url   = reverse_lazy('index')
+
+class AndernBenutzerView(UpdateView):
+    model = get_user_model()
+    template_name = 'Zugriff/andern_Benutzer.html'
+    fields = ['name', 'email']
+    success_url   = reverse_lazy('Zugriff:index')
+
+    def get_object(self):
+        return self.request.user
